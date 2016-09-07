22a8e0fbe5a7149bdeb694b322b37d81a9cbf040
Eric Mouledoux <EricMouledoux@gmail.com>
2016-09-07 09:15:04 -0500

    Messenger

--- MouledouxUtilities/MouledouxUtilities/MessengerSystem/Messenger.cs
+++ MouledouxUtilities/MouledouxUtilities/MessengerSystem/Messenger.cs
@@ -1,5 +1,6 @@
 ﻿namespace Mouledoux.Interaction
 {
+
     internal interface IPublisher
     {
     }
@@ -14,15 +15,19 @@
 
     internal static class Messenger
     {
+        public static int SubscribeTo(string aMessage)
+        {
+
+            return 1;
+        }
+
         public static int BroadcastMessage(string aMessage)
         {
             return 1;
         }
 
-        private static System.Collections.Generic.Dictionary<string, System.Delegate> m_messages =
-                    new System.Collections.Generic.Dictionary<string, System.Delegate>();
-
-        private delegate int Callback();
+        //private static System.Collections.Generic.Dictionary<string, System.Action<int> m_messages =
+        //            new System.Collections.Generic.Dictionary<string, System.Action();
     }
 
 
