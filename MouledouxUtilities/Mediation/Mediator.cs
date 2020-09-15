5e25ba264c9c76f0833fe72f2b87222f4a099149
Eric <EricMouledoux@gmail.com>
2020-09-15 16:43:54 -0500

    Update Mediator.cs
    FIXED CASTING T TO TYPE

--- MouledouxUtilities/Mediation/Mediator.cs
+++ MouledouxUtilities/Mediation/Mediator.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using System.Collections.Generic;
 using Mouledoux.Mediation.Components;
@@ -16,14 +18,44 @@ namespace Mouledoux.Mediation.Systems
 
             foreach (Type type in m_typedMessages[a_message])
             {
-                if (type == typeof(T))
+                if (HasGetImplicitConversion(type, typeof(T), out MethodInfo o_implicit))
                 {
-                    typeof(Catalogue<>).MakeGenericType(new Type[] { type }).
-                        GetMethod("NotifySubscribers").Invoke(null,
-                            new object[] { a_message, a_arg, a_holdMessage });
+                    Type thisType = typeof(Catalogue<>).MakeGenericType(new Type[] { type });
+                    MethodInfo typedMethod = thisType.GetMethod("NotifySubscribers");
+
+                    dynamic arg = o_implicit == null ? a_arg : o_implicit.Invoke(null, new object[] { a_arg });
+                    typedMethod.Invoke(null, new object[] { a_message, arg, a_holdMessage });
                 }
             }
         }
+
+        private static bool HasGetImplicitConversion(Type a_baseType, Type a_targetType, out MethodInfo o_method)
+        {
+            if (a_baseType == a_targetType)
+            {
+                o_method = default;
+                return true;
+            }
+
+            MethodInfo[] methods = a_baseType.GetMethods(BindingFlags.Public | BindingFlags.Static);
+            IEnumerable<MethodInfo> implicitCasts = methods.Where(mi => mi.Name == "op_Implicit" && mi.ReturnType == a_baseType);
+            bool hasMatchingCast = implicitCasts.Any(mi =>
+            {
+                ParameterInfo pi = mi.GetParameters().FirstOrDefault();
+                return pi != null && pi.ParameterType == a_targetType;
+            });
+
+            o_method = implicitCasts.FirstOrDefault();
+            return hasMatchingCast;
+        }
+
+
+
+
+
+
+
+
 
         public static void NotifySubscribersAsync<T>(string a_message, T a_arg, bool a_holdMessage = false)
         {
@@ -32,7 +64,7 @@ namespace Mouledoux.Mediation.Systems
         }
 
 
-        private static void TryAddTypedMessage(string a_message, Type a_type)
+        public static void TryAddTypedMessage(string a_message, Type a_type)
         {
             if (!m_typedMessages.ContainsKey(a_message))
             {
