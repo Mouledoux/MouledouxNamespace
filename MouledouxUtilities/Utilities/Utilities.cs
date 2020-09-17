4798c9bd2722382ed36d5177536397dfcb60d32e
using namespace <EricMouledoux@Gmail.com>
2020-09-16 21:09:26 -0500

    Create Utilities.cs
    new class for non-specific helper functions

---
+++ MouledouxUtilities/Utilities/Utilities.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mouledoux.Utilities
+{
+    public static class Utilities
+    {
+        public static void ValidateDelegateCallback(ref Delegate a_delegate)
+        {
+            foreach (Delegate del in a_delegate.GetInvocationList())
+            {
+                Delegate.Remove(a_delegate, del.Target.Equals(null) ? del : default);
+            }
+        }
+    }
+}
