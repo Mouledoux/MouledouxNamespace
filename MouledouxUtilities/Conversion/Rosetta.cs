cc9f90652919b77b3912ce18678761562b74f2b5
Mouledoux <Eric.Mouledoux@Nabors.com>
2020-09-17 11:06:57 -0500

    Update Rosetta.cs
    more methods!

--- MouledouxUtilities/Conversion/Rosetta.cs
+++ MouledouxUtilities/Conversion/Rosetta.cs
@@ -15,8 +15,7 @@ namespace Mouledoux.Conversion
             return hasCast;
         }
 
-
-        public static bool TryPerformImplicitCastFromTo<T, U>(T a_obj, out U o_obj)
+        public static bool TryPerformImplicitCastFromTo<T, U>(ref T a_obj, out U o_obj)
         {
             bool hasCast = TryGetImplicitCastFromTo<T, U>(out MethodInfo implicitCast);
             o_obj = hasCast ? implicitCast.Invoke(null, new object[] { a_obj }) as dynamic : default;
@@ -24,34 +23,46 @@ namespace Mouledoux.Conversion
         }
 
 
+        public static bool TryGetAnyCastFromTo<T, U>(out MethodInfo o_method)
+        {
+            return TryGetAnyStaticCastFromTo<T, U>(out o_method);
+        }
+
         public static bool TryGetExplicitCastFromTo<T, U>(out MethodInfo o_method)
         {
             return TryGetMethodCastFromTo<T, U>("op_Explicit", out o_method);
         }
-
 
         public static bool TryGetImplicitCastFromTo<T, U>(out MethodInfo o_method)
         {
             return TryGetMethodCastFromTo<T, U>("op_Implicit", out o_method);
         }
 
+        public static bool TryGetMethodCastFromTo<T, U>(string a_methodName, out MethodInfo o_method)
+        {
+            return TryGetAnyStaticCastFromTo<T, U>(out o_method, a_methodName);
+        }
 
-        private static bool TryGetMethodCastFromTo<T, U>(string a_methodName, out MethodInfo o_method)
+
+
+
+        private static bool TryGetAnyStaticCastFromTo<T, U>(out MethodInfo o_method, string a_methodName = default)
         {
             Type origin = typeof(T);
             Type target = typeof(U);
 
             MethodInfo[] methods = target.GetMethods(BindingFlags.Public | BindingFlags.Static);
-            IEnumerable<MethodInfo> cast = methods.Where(mi => mi.Name == a_methodName && mi.ReturnType == target);
-            bool hasCast = cast.Any(mi =>
-            {
-                ParameterInfo pi = mi.GetParameters().FirstOrDefault();
-                return pi != null && pi.ParameterType == origin;
-            });
+            IEnumerable<MethodInfo> cast = methods.Where(mi =>
+                mi.ReturnType == target &&
+                (a_methodName == default || mi.Name == a_methodName) &&
+                mi.GetParameters().FirstOrDefault().ParameterType == origin);
 
             o_method = cast.FirstOrDefault();
-            return hasCast;
+            return cast.Count() > 0;
         }
+
+
+
 
     }
 }
